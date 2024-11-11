35f38dba3369261fbc1cdf8fb2404bb4adea1c0e
Sopi <sopran.patrik@gmail.com>
2024-11-11 11:51:12 +0100

    add qr code

--- app/javascript/components/chatroom.jsx
+++ app/javascript/components/chatroom.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import React from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 import { chatroomStyle, messagesStyle, newMessageStyle } from '../styles/styles.js'
 import { useState, useEffect } from 'react'
 import useChatStore from '../data/chat_store.js'
 import { Message } from './message.jsx'
+import  { QrCode }  from './qr.jsx'
 
 const Chatroom = () => {
-
   const [currentUser, setCurrentUser] = useState(sessionStorage.getItem('userName'))
-  const { chatroomName } = useParams();
+  const { chatroomName } = useParams()
   const [message, setMessage]  = useState('')
 
   const connect = useChatStore((state) => state.connect)
@@ -23,6 +23,7 @@ const Chatroom = () => {
 
   const sendMessage = (async (event) => {
     if(event.key != 'Enter') { return }
+    if(message == '') { return }
 
     const response = await fetch(new URL('http://localhost:3000/api/messages'), {
       method: 'post',
@@ -66,11 +67,12 @@ const Chatroom = () => {
   return (
   <div style={chatroomStyle}>
     <h1>Hi {currentUser}, welcome to the chatroom {chatroomName}</h1>
+    <QrCode/>
     <input style={newMessageStyle} type='text' placeholder='what do you want to say?' value={message} onChange={updateMessage} onKeyPress={sendMessage} />
     <div>
       { messages.sort((a, b) => b.time - a.time).map((message, index) => <Message key={index} userName={message.user} message={message.message} index={index}/>)}
     </div>
-    </div>
+  </div>
   )
 };
 
---
+++ app/javascript/components/qr.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { QRCodeSVG } from 'qrcode.react'
+import { useState, useEffect } from 'react'
+
+export const QrCode = () => {
+  const [showQrCode, setShowQrCode] = useState(true)
+
+  const [isQPressed, setIsQPressed] = useState(false)
+  const [isRPressed, setIsRPressed] = useState(false)
+  const holdDuration = 1000
+
+  useEffect(() => {
+    let timer;
+
+    const handleKeyDown = (event) => {
+      if (event.key === 'q') setIsQPressed(true);
+      if (event.key === 'r') setIsRPressed(true);
+    };
+
+    const handleKeyUp = (event) => {
+      if (event.key === 'q') setIsQPressed(false);
+      if (event.key === 'r') setIsRPressed(false);
+    }
+
+    if (isQPressed && isRPressed) {
+      timer = setTimeout(() => {
+        setShowQrCode(true)
+      }, holdDuration);
+    } else {
+      clearTimeout(timer)
+      setShowQrCode(false)
+    }
+
+    // Attach keydown and keyup event listeners to the document
+    document.addEventListener('keydown', handleKeyDown)
+    document.addEventListener('keyup', handleKeyUp)
+
+    // Cleanup event listeners and clear the timer on component unmount
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+      document.removeEventListener('keyup', handleKeyUp)
+      clearTimeout(timer)
+    };
+  }, [isQPressed, isRPressed])
+
+  return (
+    <div>
+    {
+      showQrCode && (
+        <QRCodeSVG value={window.location.href} />
+      )
+    }
+    </div>
+  )
+};
