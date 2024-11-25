11fa01d25bac396cbe9b3d75ff59caeb448181d1
Sopi <sopran.patrik@gmail.com>
2024-11-25 13:43:49 +0100

    fix singleton pattern and thread check

--- app/services/sse_manager.rb
+++ app/services/sse_manager.rb
@@ -15,8 +15,8 @@ class SseManager
   def self.add_connection(connection)
     return unless connection.is_a?(Connection)
 
-    instance.start_thread unless @thread.present? && @thread.alive?
     instance.connection_queue << connection
+    instance.start_thread unless instance.thread_alive?
   end
 
   def self.active_connection_ids
@@ -24,38 +24,40 @@ class SseManager
   end
 
   def start_thread
-    @thread = Thread.new do
-      conn = ActiveRecord::Base.connection.raw_connection # connect to database
-      conn.async_exec('LISTEN chat_messages') # listen to data sent in the chat_messages_channel
+    @thread_alive = true
+    Thread.new do
+      ActiveRecord::Base.connection_pool.with_connection do |conn|
+        conn.raw_connection.async_exec('LISTEN chat_messages') # listen to data sent in the chat_messages_channel
 
-      connections = []
+        connections = []
 
-      loop do
-        # 1) ensure connections are added and cleaned up
-        connections = prepare_connections(connections)
-        @active_connection_ids = connections.map(&:id).uniq
-        #@thread.exit if connections.empty?
-
-        # 2. process pg noticifactions
-        # returns the channel name if message received within timeout, else nil
-        conn.wait_for_notify(30) do |_channel, _pid, payload|
-          message = Message.from_json(JSON.parse(payload.to_s))
-
-          connections.each do |connection|
-            connection.write(message)
-          rescue IOError, SocketError, Errno::EPIPE, Errno::ECONNRESET
-            connection.close
-            next
+        loop do
+          # 1) ensure connections are added and cleaned up
+          connections = prepare_connections(connections)
+          @active_connection_ids = connections.map(&:id).uniq
+          if connections.empty?
+            @thread_alive = false
+            Thread.current.exit
           end
-          # 3. if no message came through, check if any connections have been disconnected
-        end || connections.each do |connection|
-          connection.check_if_alive
-        rescue IOError, SocketError, Errno::EPIPE, Errno::ECONNRESET
-          connection.close
-          next
+          @thread.exit if connections.empty?
+
+          # 2. process pg noticifactions
+          # returns the channel name if message received within timeout, else nil
+          conn.raw_connection.wait_for_notify(30) do |_channel, _pid, payload|
+            message = Message.from_json(JSON.parse(payload.to_s))
+
+            io_for_each_connection(connections) do |connection|
+              connection.write(message)
+            end
+            # 3. if no message came through, check if any connections have been disconnected
+          end || io_for_each_connection(connections, &:check_if_alive)
         end
       end
     end
+  end
+
+  def thread_alive?
+    @thread_alive || false
   end
 
   private
@@ -74,5 +76,14 @@ class SseManager
     connections.reject!(&:closed?)
 
     connections
+  end
+
+  def io_for_each_connection(connections)
+    connections.each do |connection|
+      yield(connection) if block_given?
+    rescue IOError, SocketError, Errno::EPIPE, Errno::ECONNRESET
+      connection.close
+      next
+    end
   end
 end
