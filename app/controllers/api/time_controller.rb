3e5b71766f5caabf50d9c2b29815b3cbf8984282
Sopi <sopran.patrik@gmail.com>
2024-11-27 08:14:27 +0100

    apply rack hijacking on main

--- app/controllers/api/time_controller.rb
+++ app/controllers/api/time_controller.rb
@@ -3,21 +3,39 @@
 # thanks to https://pragmaticpineapple.com/using-server-sent-events-to-stream-data-in-rails/
 module Api
   class TimeController < ApplicationController
-    include ActionController::Live # include this to use the SSE object
-
     def show
-      response.headers['Content-Type'] = 'text/event-stream' # make sure we provide the right response format
-      sse = SSE.new(response.stream, event: 'message') # define the event, most commonly message
-      loop do
-        sse.write(Time.now.strftime('%H:%M:%S')) # get current time and write it to stream
-        sleep 1
+
+      request.env['rack.hijack'].call
+      stream = request.env['rack.hijack_io']
+      send_headers(stream)
+      Thread.new do
+        loop do
+          stream.write("event: message\ndata: #{Time.now.strftime('%H:%M:%S')}\n\n") # get current time and write it to stream
+          sleep 1
+        rescue  StandardError
+          stream.close
+        end
       end
+
+      response.close
     rescue StandardError => e
       Rails.logger.error 'closed sse due to an error'
       Rails.logger.error e
-      sse.close
-    ensure
-      sse.close # make sure we don't have too many connections open (keep this in mind for later)
+    end
+
+    private
+
+    def send_headers(stream)
+      headers = [
+        "HTTP/1.1 200 OK",
+        "Content-Type: text/event-stream"
+      ]
+      stream.write(headers.map { |header| header + "\r\n" }.join)
+      stream.write("\r\n")
+      stream.flush
+    rescue
+      stream.close
+      raise
     end
   end
 end
